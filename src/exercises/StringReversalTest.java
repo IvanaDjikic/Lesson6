daf3a3e43e8cad5f1cd8fed9be19dc9d22fc174d
IvanaDjikic <ivdjikic@gmail.com>
2019-05-26 21:42:45 +0200

    StringreversalTest change

---
+++ src/exercises/StringReversalTest.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class StringReversalTest {
+
+	public static void main(String[] args) {
+
+		System.out.println(new StringReversal().reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+
+	}
+
+}
