36778714255f8bc2408752beffb00303c4ac64ef
IvanaDjikic <ivdjikic@gmail.com>
2019-05-26 21:41:40 +0200

    StringReversal change

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class StringReversal extends TestCase {
@@ -7,12 +9,31 @@ public class StringReversal extends TestCase {
 	 * Without using the reverse() method in StringBuffer or StringBuilder, write a
 	 * string reverser that will make this unit test pass.
 	 * 
-	 * Then use it to decode this message: 
-	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
+	 * Then use it to decode this message: QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	String termToReverse;
+	String reversedTerm;
+	ArrayList<Character> chars = new ArrayList<Character>();
+	ArrayList<Character> reversedChars = new ArrayList<Character>();
+
+	String reverseMe(String termToReverse) {
+		for (int i = 0; i < termToReverse.length(); i++) {
+
+			chars.add(termToReverse.charAt(i));
+		}
+		for (int i = chars.size() - 1; i >= 0; i--) {
+			reversedChars.add(chars.get(i));
+		}
+		reversedTerm = reversedChars.toString().replaceAll("[,\\s\\[\\]]", "");
+
+		return reversedTerm;
+
+	}
+	
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+	}
+	
 
 }
